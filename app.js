516a4f15aa10a7641d74ed9c34885cfbcf8d0075
Luís Carlos G. <137448502+Luis-60@users.noreply.github.com>
2025-03-09 22:22:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ document.querySelectorAll('.progressbar span').forEach(function(span) {
     // Define a largura do span com base no valor do data-width
     span.style.width = progressWidth + '%';
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector(".menu");
+    const offScreenMenu = document.querySelector(".off-screen-menu");
+    const overlay = document.querySelector(".overlay");
+
+    menuBtn.addEventListener("click", function () {
+        offScreenMenu.classList.toggle("active");
+        menuBtn.classList.toggle("active");
+        overlay.classList.toggle("active"); // Ativa/desativa o overlay
+    });
+
+    // Fecha o menu ao clicar no overlay
+    overlay.addEventListener("click", function () {
+        offScreenMenu.classList.remove("active");
+        menuBtn.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
