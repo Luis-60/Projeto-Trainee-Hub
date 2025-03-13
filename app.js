206777990e11d76cd2acad2bf7847291c37b0968
Luís Carlos G. <137448502+Luis-60@users.noreply.github.com>
2025-03-13 06:14:21 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -35,3 +35,22 @@ document.addEventListener("DOMContentLoaded", function () {
         overlay.classList.remove("active");
     });
 });
+
+//Accordion Panel
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Alternar entre adicionar e remover a classe "active", para destacar o botão que controla o painel */
+    this.classList.toggle("active-accordion");
+	
+    /* Alternar entre ocultar e mostrar o painel ativo */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
