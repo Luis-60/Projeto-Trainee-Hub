7837ff9eff1f8dfb8c4e4a0cfbf85536321b1f86
Luís Carlos G. <137448502+Luis-60@users.noreply.github.com>
2025-03-15 03:49:35 -0300

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_Trainee_Hub.Models;
+
+namespace Projeto_Trainee_Hub.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public AdminController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult SSTreinamentos()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
